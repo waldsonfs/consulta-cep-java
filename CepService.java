0a1692059b9a3df20a08f62076f7539dddbff92f
Waldson Fernandes <waldsonfernandes043@gmail.com>
2026-03-04 14:05:35 -0300

    Implementação do CepService para consumo da API ViaCEP

--- CepService.java
+++ CepService.java
@@ -1,6 +1,24 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class CepService {
     public String getDadosCep(String cep) throws Exception{
 
+        String cepFormatado = cep.replaceAll("[^0-9]", "");
+
+        String apiUrl = "https://viacep.com.br/ws/" + 
+        cepFormatado + "/json/";
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(apiUrl)).build();
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
 
     }
 }
